9615c04cbb68e8d0980d6ebc9c5a12f069bb00c9
Martin Etmajer <martin.etmajer@compuware.com>
2014-12-15 16:35:51 +0100

    create-ssh-keypair.sh script creates missing keys directory if necessary.

--- bin/create-ssh-keypair.sh
+++ bin/create-ssh-keypair.sh
@@ -5,6 +5,7 @@ PASSWORD_FILE_NAME="$KEY_NAME.password"
 #PASSWORD=`date +%s | shasum | base64 | head -c 8`
 PASSWORD=
 
+mkdir -p keys
 ssh-keygen -q -t rsa -f keys/$KEY_NAME -N "$PASSWORD" -C "$KEY_COMMENT"
 echo $PASSWORD > keys/$PASSWORD_FILE_NAME
 chmod 600 keys/$KEY_NAME*
