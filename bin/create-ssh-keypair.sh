a6ebaf3fc230d7436adb2dda07494322c618619d
Martin Etmajer <martin.etmajer@compuware.com>
2015-01-26 12:50:18 +0100

    Public/private key-pair creation uses randomly generated passwords now.

--- bin/create-ssh-keypair.sh
+++ bin/create-ssh-keypair.sh
@@ -2,12 +2,9 @@ KEY_NAME=$1
 KEY_COMMENT="$KEY_NAME User Public Key"
 
 PASSWORD_FILE_NAME="$KEY_NAME.password"
-#PASSWORD=`date +%s | shasum | base64 | head -c 8`
-PASSWORD=
+PASSWORD=`date +%s | shasum | base64 | head -c 8`
 
 mkdir -p keys
 ssh-keygen -q -t rsa -f keys/$KEY_NAME -N "$PASSWORD" -C "$KEY_COMMENT"
 echo $PASSWORD > keys/$PASSWORD_FILE_NAME
 chmod 600 keys/$KEY_NAME*
-
-mv keys/$KEY_NAME.pub ansible/keys
